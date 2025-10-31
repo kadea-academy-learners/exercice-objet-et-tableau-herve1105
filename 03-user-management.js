c366f269c9cf4b70e50bf91c3adedeb3cb948fdf
herve1105 <mazumbubiyela@users.noreply.github.com>
2025-10-31 18:25:51 +0100

    feat: Adds the whoIsAdmin function to filter and display admin users

--- 03-user-management.js
+++ 03-user-management.js
@@ -5,12 +5,48 @@
 // - estAdmin (boolean)
 // La fonction doit retourner un tableau contenant les noms des utilisateurs qui sont admin.
 
-function whoIsAdmin() {
-	
+function whoIsAdmin(utilisateurs) {
+
+	const admins = utilisateurs.filter(user => user.estAdmin === true);
+
+	return admins.map(user => user.nom);
 }
 
+const utilisateurs =[
+	{ nom: "Alice", age: 25, estAdmin: true },
+	{ nom: "Bob", age: 30, estAdmin: false },
+	{ nom: "Charlie", age: 28, estAdmin: true },
+];
+
+console.log(whoIsAdmin(utilisateurs));
+
+
+// 	if (!Array.isArray(users)) return [];
+
+// 	return users
+//         .filter(user => user.estAdmin === true)
+//         .map(user => user.nom);
+// }
+
+
+// const users = [
+//     { nom: "Alice", age: 25, estAdmin: true },
+//     { nom: "Bob", age: 30, estAdmin: false },
+//     { nom: "Charlie", age: 28, estAdmin: true }
+// ];
+
+// console.log(whoIsAdmin([
+//     { nom: "Alice", age: 25, estAdmin: true },
+//     { nom: "Bob", age: 30, estAdmin: false },
+//     { nom: "Charlie", age: 28, estAdmin: true }
+// ]));
+
+// console.log(whoIsAdmin([]));
+// console.log(whoIsAdmin([{ nom: "Bob", age: 30, estAdmin: false }]));
+// console.log(whoIsAdmin([{ nom: "Alice", age: 30, estAdmin: true }]));
+
+
+// Export pour les tests si nécessaire
 module.exports = {
 	whoIsAdmin,
 };
-
-
